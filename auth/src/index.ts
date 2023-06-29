40a416aa8a99f80765082db89acf0957244f07ae
despau <raugustin1049@gmail.com>
2023-06-29 13:02:55 -0400

    adding user to database

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -8,6 +9,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+
 
 const app = express();
 app.use(json());
@@ -23,6 +25,19 @@ app.all('*', async ()=> {
 
 app.use(errorHandler);
 
-app.listen(3000, ()=> {
-    console.log("Listening on port 3000 auth !!!")
-});
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MONGO-DB')
+
+    } catch (err) {
+        console.error(err);
+    }
+    
+    app.listen(3000, ()=> {
+        console.log("Listening on port 3000 auth !! :P ")
+    });
+}
+
+start();
+
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+
+import { User } from './../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -22,12 +23,21 @@ router.post('/api/users/signup',
             throw new RequestValidationError(errors.array());
         }
 
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            console.error('Email in Use');
+            return res.send({});
+        }
+
+        const user = User.build({ email, password })
+        await user.save();
+
+        console.error('User Created');
+        res.status(201).send(user);
         
-        console.log('Creating a user .....');
-
-        throw new DatabaseConnectionError();
-
-        res.send({})
 
     }
 );
