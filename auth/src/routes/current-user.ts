ed48e689c632b2d601511dd6e9d81525c2ba464a
despau <raugustin1049@gmail.com>
2023-06-29 22:51:27 -0400

    finished with current user. now working on user sign out.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+import { User } from './../models/user';
+import { Password } from '../services/password';
+import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res)=>{
-    res.send('Hi there current user');
+router.get('/api/users/currentuser', (req: Request, res: Response)=>{
+    
+    if ( !req.session?.jwt ) {
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify( 
+            req.session.jwt,
+            process.env.JWT_KEY!
+        );
+        
+        res.send({ currentUser: payload });
+
+    } catch (err) {
+        res.send({ currentUser: null })
+    }
+
 });
 
 export { router as currentUserRouter };
