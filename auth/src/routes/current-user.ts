b0882e588c50e018368ce114ca66616ae70f7350
despau <raugustin1049@gmail.com>
2023-06-29 23:07:55 -0400

    finished with current user middleware. starting with auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,31 +1,12 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-import { User } from './../models/user';
-import { Password } from '../services/password';
-import { BadRequestError } from '../errors/bad-request-error';
-import { validateRequest } from '../middlewares/validate-request';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response)=>{
-    
-    if ( !req.session?.jwt ) {
-        return res.send({ currentUser: null });
-    }
-
-    try {
-        const payload = jwt.verify( 
-            req.session.jwt,
-            process.env.JWT_KEY!
-        );
-        
-        res.send({ currentUser: payload });
-
-    } catch (err) {
-        res.send({ currentUser: null })
-    }
-
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response)=>{
+    res.send( { currentUser: req.currentUser || null } );
 });
 
 export { router as currentUserRouter };
