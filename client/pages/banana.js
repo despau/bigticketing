699ce8157701e815a119332146e55f27906b0589
despau <raugustin1049@gmail.com>
2023-07-04 15:45:45 -0400

    created client/auth interface for signup. creating a helper method.

---
+++ client/pages/banana.js
@@ -0,0 +1,7 @@
+const Banana = () => {
+    return (
+        <h3>Banana2</h3>
+    )
+}
+
+export default Banana;
---
+++ client/pages/index.js
@@ -0,0 +1,6 @@
+export default () => {
+    return (
+        <h1>Landing Page</h1>
+    )
+}
+
