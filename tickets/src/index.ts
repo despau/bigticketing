c0050099d45847bd9accbd9a7961cbd58bfeea9f
despau <raugustin1049@gmail.com>
2023-07-05 23:25:14 -0400

    setting up tickets service and env variable to database connection

---
+++ tickets/src/index.ts
@@ -0,0 +1,31 @@
+
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+
+const start = async () => {
+
+    if (!process.env.JWT_KEY) {
+        throw new Error('Missing JWT_KEY. It must be defined.')
+    }
+
+    if (!process.env.MONGO_URI) {
+        throw new Error('Missing MONGO_URI. It must be defined.')
+    }
+    
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MONGO-DB')
+
+    } catch (err) {
+        console.error(err);
+    }
+    
+    app.listen(3000, ()=> {
+        console.log("Listening on port 3000 auth !! :P ")
+    });
+}
+
+start();
+
